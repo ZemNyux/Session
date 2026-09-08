58e46f7ba3f18803ec1190b86af05b083fdb2313
ZemNyux <nikita.gaber2703@gmail.com>
2026-09-08 17:42:03 +0300

    Cookies ASP.NET Core

---
+++ Downloads/aspnetcore_cookie-master/aspnetcore_cookie-master/Cookie/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cookie.Models;
+
+namespace Cookie.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                var option = new CookieOptions();
+                option.Expires = DateTime.Now.AddDays(10); // термін збереження кукі - 10 днів
+                Response.Cookies.Append("login", login.UserName, option); // створення кукі
+                return RedirectToAction("Index", "Home");
+            }
+            return View(login);
+        }
+    }
+}
---
+++ Downloads/aspnetcore_cookie-master/aspnetcore_cookie-master/Cookie/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cookie.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Поле має бути встановлене")]
+        [Display(Name = "Ім'я користувача: ")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Поле має бути встановлене")]
+        [Display(Name = "Пароль: ")]
+        public string? Password { get; set; }
+    }
+}
