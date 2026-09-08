4f4f23bb1eac432193f1340c8e477f34caf215d4
ZemNyux <nikita.gaber2703@gmail.com>
2026-09-08 19:04:47 +0300

    Session Example

---
+++ Downloads/aspnetcore_session-master/aspnetcore_session-master/Cookie/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // перевірка, чи користувач авторизований
+            if (HttpContext.Session.GetString("login") == null)
+
+            // if (!User.Identity.IsAuthenticated) - взагалі, для авторизації є окрема сучасна мідлварь, якщо налаштувати приблизно так:
+            // в класі Program буде builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            //.AddCookie(options =>
+            //{
+            //    options.LoginPath = "/Account/Login"; // куди редиректимо неавторизованих користувачів
+            //    options.AccessDeniedPath = "/Account/AccessDenied";
+            //});
+            // builder.Services.AddAuthorization();
+            // в ланцюжок мідлварєй додаємо
+            // app.UseAuthentication(); // спочатку Authentication
+            // app.UseAuthorization(); // потім Authorization, але цей приклад більш універсальний, в сесії можна зберігати багато чого :)
+            {
+                return RedirectToAction("Create", "Login");
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear(); // очищення сесії
+            return RedirectToAction("Create", "Login");
+        }
+    }
+}
---
+++ Downloads/aspnetcore_session-master/aspnetcore_session-master/Cookie/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Session.Models;
+
+namespace Session.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpContext.Session.SetString("login", login.UserName); // створення сесійної змінної (доречі, саму сесію в асп не треба створювати)
+                return RedirectToAction("Index", "Home");
+            }
+            return View(login);
+        }
+    }
+}
